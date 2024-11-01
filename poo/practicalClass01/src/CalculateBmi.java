9a434787f21993bd7d46502887e076a0513fd9e9
Gabriela <gabisantosdeveloper@gmail.com>
2024-11-01 07:57:04 -0300

    feat(poo): implement first practival class exercises

---
+++ poo/practicalClass01/src/CalculateBmi.java
@@ -0,0 +1,14 @@
+
+// Obtenha a entrada padrão peso (int), a altura (double) e imprima o IMC (Índice de Massa Corporal).
+//
+//        - IMC = peso em quilogramas dividido pela altura em metros elevada ao quadrado
+//
+//        \[
+//        \text{IMC} = \frac{\text{PESO}}{\text{ALTURA}^2}
+//        \]
+
+public class CalculateImc {
+    public void calculateImc(){
+
+    }
+}
